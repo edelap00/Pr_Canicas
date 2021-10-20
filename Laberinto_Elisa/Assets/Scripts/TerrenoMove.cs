bfe6b1c3d7c54979ad2a8cd2cb6ea11edc1639f0
Elisa <elisadelap00@gmail.com>
2021-10-20 12:41:29 +0200

    Fin por hoy ala, repositorio cerrado

--- Laberinto_Elisa/Assets/Scripts/TerrenoMove.cs
+++ Laberinto_Elisa/Assets/Scripts/TerrenoMove.cs
@@ -6,6 +6,12 @@ public class TerrenoMove : MonoBehaviour
 {
     public Rigidbody rb;
     [SerializeField] float fuerza;
+    [SerializeField] float limRight = 0.15f;
+    [SerializeField] float limLeft = -0.15f;
+    [SerializeField] float limUp = 0.15f;
+    [SerializeField] float limDown = -0.15f;
+    bool inLimitX;
+    [SerializeField] bool inLimitZ = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,13 +29,44 @@ public class TerrenoMove : MonoBehaviour
 
     void Girar()
     {
-        //mov eje X
-        float desplX = Input.GetAxis("Horizontal") * fuerza;
-        // transform.Translate(Vector3.right * desplX * Time.deltaTime);
-        rb.AddTorque(new Vector3(0.2f, 0, 0) * desplX);
-        //mov eje Y
+       // transform.rotation.y = new Vector3((transform.rotation.x, 0, transform.rotation.z));
+       //mov ejes
+        float desplX = Input.GetAxis("Horizontal") * fuerza; 
         float desplZ = Input.GetAxis("Vertical") * fuerza;
-        //transform.Translate(Vector3.forward * desplZ * Time.deltaTime);
-        rb.AddTorque(new Vector3(0, 0, 0.2f) * desplZ);
+
+        //código para restricción
+
+        float rotX = transform.rotation.x;
+        float rotZ = transform.rotation.z;
+
+        if (rotX >= limRight && desplX > 0 || rotX <= limLeft && desplX < 0)
+        {
+            inLimitX = false;
+        }
+        else { inLimitX = true; }
+
+        if (inLimitX)
+        {
+            //mov eje X
+           
+            // transform.Translate(Vector3.right * desplX * Time.deltaTime);
+            rb.AddTorque(new Vector3(0.2f, 0, 0) * desplX);
+        }
+
+        if (rotZ >= limUp && desplZ > 0 || rotZ <= limDown && desplZ < 0)
+        {
+            inLimitZ = false;
+        }
+        else { inLimitZ = true; }
+
+        if (inLimitZ)
+        {
+            //mov eje Z
+           
+            //transform.Translate(Vector3.forward * desplZ * Time.deltaTime);
+            rb.AddTorque(new Vector3(0, 0, 0.2f) * desplZ);
+        }
+
+       
     }
 }
